66b53391fb070a039e48b7a1962df4ee55d36519
Andre-DevMarques <andremarquesfiih@gmai.com>
2024-11-29 15:53:47 -0300

    Primeira verção

---
+++ arquivo.js
@@ -0,0 +1,21 @@
+let button = document.querySelector("#button");
+
+button.addEventListener("click", function() {
+    let PrimeiroInput = document.querySelector("#input1").value;
+    let SegundoInput = document.querySelector("#input2").value;
+    let resultado = document.querySelector("#resultado");
+
+    // Converte os valores para números
+    PrimeiroInput = Number(PrimeiroInput);
+    SegundoInput = Number(SegundoInput);
+
+    // Define o valor mínimo e máximo para o sorteio
+    let min = Math.min(PrimeiroInput, SegundoInput);
+    let max = Math.max(PrimeiroInput, SegundoInput);
+
+    // Gera o número aleatório entre o valor mínimo e máximo
+    let numeroSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // Exibe o número sorteado no parágrafo
+    resultado.innerHTML = `Número sorteado: ${numeroSorteado}`;
+});
