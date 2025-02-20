5d1dd9e2416c8ddfabbac0ad7d0c582aefc7c963
Andre-DevMarques <andremarquesfiih@gmai.com>
2025-02-20 15:22:03 -0300

    Melhora no codigo

--- arquivo.js
+++ arquivo.js
@@ -5,15 +5,12 @@ button.addEventListener("click", function() {
     let SegundoInput = document.querySelector("#input2").value;
     let resultado = document.querySelector("#resultado")
 
-    // Define o valor mínimo e máximo para o sorteio
     let min = Math.min(PrimeiroInput);
     let max = Math.max(SegundoInput);
 
-    // Gera o número aleatório entre o valor mínimo e máximo
     if(max > min){
     let numeroSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    // Exibe o número sorteado no parágrafo
     resultado.innerHTML = `Número sorteado: ${numeroSorteado}`;}else{
 
         alert("O valor MINIMO não pode ser maior do que o valor maximo")
